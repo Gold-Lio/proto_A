746cff8ff6ba58c209dfbdde554c0c7d8b4d628b
Khan Choi <81938345+jk-JOY@users.noreply.github.com>
2022-06-15 12:29:28 +0900

    fixed

---
+++ Assets/Scripts/Enum & Magic/EnumState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumState : MonoBehaviour
+{
+    public static EnumState instance;
+
+    public enum NetworkScreen
+    {
+        EScreen_X = 720,
+        EScreen_Y = 405
+    }
+
+    public enum PlayerState
+
+    {
+
+    }
+
+}
