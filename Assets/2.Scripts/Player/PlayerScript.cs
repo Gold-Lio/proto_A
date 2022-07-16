55f63f7833f75786aa77cd472d690ab686153070
진기 <jingit38@gmail.com>
2022-07-17 01:12:26 +0900

    anim

--- Assets/2.Scripts/Player/PlayerScript.cs
+++ Assets/2.Scripts/Player/PlayerScript.cs
@@ -12,9 +12,7 @@ using Random = UnityEngine.Random;
 public class PlayerScript : MonoBehaviourPunCallbacks ,  IPunObservable
 {
     public static PlayerScript PS;
-
     public Inventory inventory;
-    //public bl_Joystick js;
 
     public Rigidbody2D RB;
     public SpriteRenderer SR;
@@ -41,13 +39,7 @@ public class PlayerScript : MonoBehaviourPunCallbacks ,  IPunObservable
 
     public GameObject playerCanvasGo;
 
-    // 2022.06.26 kkh : hp 슬라이더 추가
-    private Slider hp_slider = null;
-    private float hp_Max = 100.0f;
-    private float hp_Cur = 100.0f;
-    public float HP_Cur { get => hp_Cur; set => hp_Cur = value; }
-
-
+    public Animator anim;
 
     private void Awake()
     {
@@ -63,13 +55,14 @@ public class PlayerScript : MonoBehaviourPunCallbacks ,  IPunObservable
         NM.Players.Add(this);
         NM.SortPlayers();
         isMove = true;
+        anim = GetComponent<Animator>();
+
     }
 
     public Vector3 GetPosition()
     {
         return transform.position;
     }
-
     void SetNick()
     {
         NickText.text = PV.IsMine ? PhotonNetwork.NickName : PV.Owner.NickName;
@@ -88,31 +81,24 @@ public class PlayerScript : MonoBehaviourPunCallbacks ,  IPunObservable
                 input = new Vector2(inputX, inputY);
                 input *= speed;
                 RB.velocity = input.normalized * speed;
+                if(inputX > 0 && inputY > 0 )
+                {
+                    anim.SetBool("Walk", true);
+                }
+                else if(inputX < 0 && inputY < 0)
+                {
+                    anim.SetBool("Idle",true);
+                }
 
                 if (inputX != 0)
                 {
                     PV.RPC("FlipXRPC", RpcTarget.AllBuffered, inputX);
                 }
+
                 NM.PointLight2D.transform.position = transform.position + new Vector3(0, 0, 10);
             }
         }
 
-        // 2022.06.26 kkh : 혹시 몰라서 한번더 체크
-        //if (UM.hp_slider != null && UM.hp_slider.enabled && hp_slider == null)
-        //{
-        //    hp_slider = UM.hp_slider;
-        //}
-
-        //if (hp_slider != null && hp_slider.enabled)
-        //{
-
-        //    // 2022.06.26 kkh : 피격 당했을때 이걸 넣어야하는데 그게 어디지?
-        //    //hp_Cur -= 10;
-
-
-        //    // 2022.06.26 kkh : hp 감소 실행
-        //    HandleHP();
-        //}
         // IsMine이 아닌 것들은 부드럽게 위치 동기화
         else if ((transform.position - curPos).sqrMagnitude >= 100) transform.position = curPos;
         else transform.position = Vector3.Lerp(transform.position, curPos, Time.deltaTime * 10);
@@ -193,7 +179,6 @@ public class PlayerScript : MonoBehaviourPunCallbacks ,  IPunObservable
             curPos = (Vector3)stream.ReceiveNext();
         }
     }
-
 
     private void OnTriggerEnter2D(Collider2D col)
     {
