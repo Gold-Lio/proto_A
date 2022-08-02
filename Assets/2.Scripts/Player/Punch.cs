08be33a5e722418a6c3f7e32f49ec1f1aaa21b95
진기 <jingit38@gmail.com>
2022-08-03 03:54:29 +0900

    animOK

--- Assets/2.Scripts/Player/Punch.cs
+++ Assets/2.Scripts/Player/Punch.cs
@@ -37,6 +37,8 @@ public class Punch : MonoBehaviourPunCallbacks
             CinemachineShake.Instance.ShakeCamera(camShakeIntencity, camShakeTime);
            
             col.GetComponentInChildren<HealthBar>().hp -= attackDamage;
+            
+            col.GetComponent<PlayerScript>().anim.SetTrigger("Hited");
             Debug.Log("때렸다");
         }
     }
